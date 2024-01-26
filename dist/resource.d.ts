94c3a0abb5294dc4da79facdee30ec4a10d26c9b
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-01-27 04:49:29 +0600

    Delete dist directory
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- dist/resource.d.ts
+++
@@ -1,6 +0,0 @@
-import type { StartGPT } from "./index.js";
-export declare class APIResource {
-    protected _client: StartGPT;
-    constructor(client: StartGPT);
-}
-//# sourceMappingURL=resource.d.ts.map
